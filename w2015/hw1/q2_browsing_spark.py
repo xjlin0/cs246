525c4fe8c9d67f97ee015aad80373792cbec7227
Jack <jlin@castlighthealth.com>
2015-08-27 19:29:30 -0700

    add some comments from Jagrut speech

--- w2015/hw1/q2_browsing_spark.py
+++ w2015/hw1/q2_browsing_spark.py
@@ -6,7 +6,7 @@ def parseSingle(line):
     return [ (single, 1) for single in line.split() ]
 
 def parseSet(line, itemSetSize=2):
-    return [((itemSet[0], itemSet), 1) for itemSet in list(itertools.permutations(line.split(), itemSetSize))]
+    return [ ((itemSet[0], itemSet), 1) for itemSet in list(itertools.permutations(line.split(), itemSetSize))]
 
 s        = 100 # set support threshold
 topN     = 15  # set top items to show
@@ -19,16 +19,16 @@ singleCountRDD = (fileRDD
                   .flatMap(parseSingle)
                   .reduceByKey(lambda a, b: a + b)
                   .filter(lambda (single, count): count > s)
-                  .cache()
+                  .cache()    #or collectAsMap()
                   )
 
 confidence2RDD = (fileRDD
                   .flatMap(lambda line: parseSet(line, itemSetSize=2)) #change to 3 for itemsets size 3?
-                  .reduceByKey(lambda a, b: a + b)
-                  .map(lambda ((single, itemSet), setsCount): (single, (itemSet, float(setsCount))))
+                  .reduceByKey(lambda a, b: a + b)  #attach a filter here to reduce memory usage if needed
+                  .map(lambda ((single, itemSet), setsCount): (single, (itemSet, setsCount)))
                   .join(singleCountRDD)
-                  .map(lambda (single, ((itemSet, setsCount), singleCount)): (itemSet, setsCount / singleCount))
-                  .cache()
+                  .map(lambda (single, ((itemSet, setsCount), singleCount)): (itemSet, float(setsCount) / singleCount)) 
+                  .cache()   #or lambda (itemSet, setsCount): float(setsCount)/dictionary[ itemSet ], and dictionary[ itemSet ] is disposible now
                   )
 
 print "Top " + str(topN)
