dab839f0cf750440cca5c7e255e41f5b2024ce0f
Jack <jlin@castlighthealth.com>
2015-08-27 18:28:32 -0700

    working on Mrjob hw1 ProductRecommendation

--- w2015/hw1/q2_browsing_mrjob.py
+++ w2015/hw1/q2_browsing_mrjob.py
@@ -1,69 +1,64 @@
 # http://stackoverflow.com/questions/26082234
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-from NumLines import NumLines
-from WordsPerLine import WordsPerLine
 import itertools
-import sys
 
 s        = 100 # set support threshold
 topN     = 15  # set top items to show
-fileName = 'browsing.txt'
-#fileName = 'q2testdata.txt'  #toy input set for test
+#fileName = 'browsing.txt'
+fileName = 'q2testdata.txt'  #toy input set for test
 singleCount = None
-
-def firstJob(input_file):
-    global singleCount
-    mr_job = NumLines(args=[input_file])
-    with mr_job.make_runner() as runner:
-        runner.run()
-        singleCount = runner.get_output_dir()
-
-def secondJob(input_file):
-    mr_job = WordsPerLine(args=[intermediate,input_file])
-    with mr_job.make_runner() as runner:
-        runner.run()
-
-if __name__ == '__main__':
-    firstJob(sys.argv[1])
-    secondJob(sys.argv[1])
-
-
-
-#####################
-
-
 
 class MRProductRecommendation(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_words,
-                   combiner=self.combiner_count_words,
-                   reducer=self.reducer_count_words),
-            MRStep(reducer=self.reducer_find_max_word)
+            MRStep(mapper_init=self.init_get_single_count,
+                   mapper=self.mapper_parse_line,
+                   combiner=self.combiner_count_singles,
+                   reducer=self.reducer_count_singles,
+                   reducer_final=self.final_store_single_count),
+            MRStep(mapper=self.mapper_itemSet_count,
+                   combiner=self.combiner_count_itemSet,
+                   reducer=self.reducer_count_itemSet),
+            MRStep(mapper=self.mapper_calcualte_score)
         ]
 
+   ############ Get single item count #####
+
+    def init_get_single_count(self):
+        self.single_count = {}
+
     def mapper_parse_line(self, _, line):
-        # yield each word in the line
-        return [(single, 1) for single in line.split()]
+        for single in line.split():
+            yield single, 1
 
+    def combiner_count_singles(self, single, counts):
+        yield single, sum(counts)
 
-    def combiner_count_singles(self, word, counts):
-        # optimization: sum the words we've seen so far
-        yield word, sum(counts)
+    def reducer_count_singles(self, single, counts):
+        yield single, sum(counts)
 
-    def reducer_count_words(self, word, counts):
-        # send all (num_occurrences, word) pairs to the same reducer.
-        # num_occurrences is so we can easily use Python's max() function.
+    def final_store_single_count(self, single, counts):
+        if counts > s:
+            self.single_count[single]=counts
 
+  #############  Get item pair count ###########
+  # Do I need to put ARGV[0] as input?
+    def mapper_itemSet_count(self):
+        for itemSet in list(itertools.permutations(line.split(),2)):
+            yield itemSet, 1
 
-    # discard the key; it is just None
-    def reducer_find_max_word(self, _, word_count_pairs):
-        # each item of word_count_pairs is (count, word),
-        # so yielding one results in key=counts, value=word
+    def combiner_count_itemSet(self, itemSet, setCounts):
+        yield itemSet, sum(setCounts)
 
+    def reducer_count_itemSet(self, itemSet, setCounts):
+        yield itemSet, sum(setCounts)
 
+########### calcualte confident score   ###########
+
+    def mapper_calcualte_score(self, itemSet, setCounts):
+        yield itemSet, float(setCounts)/self.single_count[itemSet[0]]
 
 if __name__ == '__main__':
     MRProductRecommendation.run()
