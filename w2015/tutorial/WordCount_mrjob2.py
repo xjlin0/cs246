a14dcedc2a53db6a51fed9663d4fb9b25838456b
Jack <jlin@castlighthealth.com>
2015-08-30 20:44:41 -0700

    working on mrjob, not working yet

---
+++ w2015/tutorial/WordCount_mrjob2.py
@@ -0,0 +1,30 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+class MRWordFreqCount(MRJob):
+
+  def init_get_words(self):
+    self.words = {}
+
+  def get_words(self, _, line):
+    for word in WORD_RE.findall(line):
+      word = word.lower()
+      self.words.setdefault(word, 0)
+      self.words[word] = self.words[word] + 1
+
+  def final_get_words(self):
+    for word, val in self.words.iteritems():
+      yield word, val
+
+  def sum_words(self, word, counts):
+    yield word, sum(counts)
+
+  def steps(self):
+    return [MRStep(mapper_init=self.init_get_words,
+                   mapper=self.get_words,
+                   mapper_final=self.final_get_words,
+                   combiner=self.sum_words,
+                   reducer=self.sum_words)]
