80d18a2a79ca0848a553ceea07f44a046a4ec3ad
Jack <jlin@castlighthealth.com>
2015-09-11 20:30:18 -0700

    refactor MrJob for q2_browsing

--- w2015/hw1/q2_browsing_mrjob_t1.py
+++ w2015/hw1/q2_browsing_mrjob_t1.py
@@ -24,24 +24,18 @@ class MRProductRecommendation(MRJob):
     items = [ pair[1] for pair in all_values ]
     topCounter = Counter(items)
     total = topCounter['Total']
-    del(topCounter['Total'])
-    topNarray = topCounter.most_common( topN )
-    topNarray.append(('Total', total)) #so the count of single item will always at the end.
-    if topNarray and topNarray[-1][1] > s:
-      yield key, topNarray #prepare for seperation of methods in the future
+    if total > s:
+      del(topCounter['Total'])
+      topNarray = topCounter.most_common( topN )
+      topNarray.append(('Total', total)) #so the count of single item will be always at the end.
+      yield key, topNarray
 
   def mapper_store_counters(self, k1, topNarray):
     total = float( topNarray.pop(-1)[1] ) 
     local_counter = Counter()
     for k2, count in topNarray:
       local_counter[(k1, k2)] = count/total
-    #get biggiest N element #from heapq import nlargest  #http://stackoverflow.com/a/2243562/4257237  or http://sochor.co/Map-Reduce-Miniproject.html
-    topNList = local_counter.most_common(topN) 
-    yield k1, topNList
-
-  def combiner_maxN(self, _, topNarray):
-    combined = reduce(lambda a, b: a + b, topNarray)
-    yield None, Counter(dict((tuple(pair_count[0]), pair_count[1]) for pair_count in combined)).most_common( topN )
+    yield k1, local_counter.most_common( topN )
 
   def reducer_maxN(self, _, topNarray):
     combined = reduce(lambda a, b: a + b, topNarray)
@@ -53,8 +47,8 @@ class MRProductRecommendation(MRJob):
                reducer=self.reducer_groupByKey),
         MRStep(mapper=self.mapper_aggregate_tops),
         MRStep(mapper=self.mapper_store_counters,
-               combiner=self.combiner_maxN,
-               reducer=self.reducer_maxN) ]
+               combiner=self.reducer_maxN,
+               reducer=self.reducer_maxN )    ]
 
 if __name__ == '__main__':
     MRProductRecommendation.run()
